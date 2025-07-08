36d9163fcd25fc1ab2b660735217458572f634a0
nipapin <papin201212@gmail.com>
2025-07-08 13:50:16 +0200

    Enhance PartnerCard component by adding a divider prop for conditional rendering and improving layout responsiveness. Update Partners component to dynamically render PartnerCard instances from a partners array, ensuring proper spacing between cards.

--- src/components/layout/PartnerCard.tsx
+++ src/components/layout/PartnerCard.tsx
@@ -12,21 +12,32 @@ interface PartnerCardProps {
 	name: string;
 	description: string;
 	type: string;
+	divider: boolean;
 }
 
-export default function PartnerCard({ image, name, description, type }: PartnerCardProps) {
+export default function PartnerCard({ image, name, description, type, divider }: PartnerCardProps) {
 	return (
 		<Box sx={{ display: "flex", flexDirection: "column", gap: 4, width: "100%" }}>
 			<Typography sx={{ fontSize: "1.5rem", fontWeight: 600, textAlign: "center" }}>{type}</Typography>
-			<Box sx={{ display: "flex", flexDirection: { xs: "column-reverse", md: "row" }, justifyContent: "space-between", width: "100%" }}>
-				<Box sx={{ display: "flex", flexDirection: "column", gap: 2, width: { md: "500px", xs: "100%" } }}>
-					<Typography sx={{ fontSize: "2rem", fontWeight: 600 }}>{name}</Typography>
-					<Typography sx={{ fontSize: "1rem", fontWeight: 400 }}>{description}</Typography>
+			<Box
+				sx={{
+					display: "flex",
+					flexDirection: { xs: "column-reverse", md: "row" },
+					justifyContent: "space-between",
+					width: "100%",
+					alignItems: "center",
+					gap: { md: "0", xs: "1rem" }
+				}}
+			>
+				<Box sx={{ display: "flex", flexDirection: "column", gap: { md: "1rem", xs: "0.5rem" }, width: { md: "500px", xs: "100%" } }}>
+					<Typography sx={{ fontSize: { md: "2rem", xs: "1.5rem" }, fontWeight: 600, display: { xs: "none", md: "block" } }}>{name}</Typography>
+					<Typography sx={{ fontSize: { md: "1rem", xs: "0.875rem" }, fontWeight: 400, textWrap: "balance" }}>{description}</Typography>
 				</Box>
 				<Box
 					sx={{
-						width: { md: "500px", xs: "100%" },
-						height: "auto",
+						width: { md: "300px", xs: "100%" },
+						aspectRatio: `${image.width} / ${image.height}`,
+						height: { md: `calc(300px * ${image.height} / ${image.width})`, xs: "auto" },
 						display: "flex",
 						justifyContent: "center",
 						"& img": { width: "100%", height: "auto" }
@@ -35,7 +46,7 @@ export default function PartnerCard({ image, name, description, type }: PartnerC
 					<Image src={image.src} alt={image.alt} width={image.width} height={image.height} />
 				</Box>
 			</Box>
-			<Divider sx={{ my: "2rem" }} />
+			{divider && <Divider sx={{ my: "2rem" }} />}
 		</Box>
 	);
 }
--- src/components/main/Partners.tsx
+++ src/components/main/Partners.tsx
@@ -4,6 +4,64 @@ import { Box, useMediaQuery, useTheme } from "@mui/material";
 import PartnerCard from "../layout/PartnerCard";
 import Title from "../shared/Title";
 
+const partners = [
+	{
+		image: {
+			src: "/images/baltia.png",
+			width: 1648,
+			height: 498,
+			alt: "Балтия ремонт"
+		},
+		name: "Балтия ремонт",
+		description:
+			"Cервис по ремонту квартир под ключ: от проекта на бумаге до комплектации мебелью и текстилем. Компания уже более 5 лет создает комфорт и уют в квартирах и домах Калининграда и области."
+	},
+	{
+		image: {
+			src: "/images/rgr-trimed.png",
+			width: 884,
+			height: 342,
+			alt: "Российская гильдия риэлторов"
+		},
+		name: "Российская гильдия риэлторов",
+		description:
+			"Добровольное объединение профессиональных участников рынка недвижимости. Гильдия действует в соответствии с Федеральным законом «О некоммерческих организациях», другими законодательными и правовыми актами РФ, Уставом и иными внутренними нормативными документами организации."
+	},
+	{
+		image: {
+			src: "/images/bel.png",
+			width: 1392,
+			height: 1160,
+			alt: "Бэл девелопмент"
+		},
+		name: "Бэл девелопмент",
+		description:
+			"Профессиональная девелоперская компания полного цикла создана в 2002 году и уже более двух десятилетий строит высококлассные жилые и коммерческие объекты в России и странах СНГ"
+	},
+	{
+		image: {
+			src: "/images/tvn.jpg",
+			width: 556,
+			height: 376,
+			alt: "Группа компаний «ТВН»"
+		},
+		name: "Группа компаний «ТВН»",
+		description:
+			"Динамично развивающийся застройщик в Калининграде и области с 2019 года. За это время группа компаний «ТВН» успешно реализовали несколько жилых комплексов и продолжают расширять портфель проектов, включая новостройки «комфорт-класса» в г. Балтийске и г. Светлогорске."
+	},
+	{
+		image: {
+			src: "/images/re.png",
+			width: 1000,
+			height: 919,
+			alt: "Русская Европа"
+		},
+		name: "Русская Европа",
+		description:
+			"Компания строит ЭКОквартал «Русская Европа» в Калининграде — первый в России «Счастливый город». Это уникальный проект, который объединяет экологичность, инновационные технологии и комфортную жизнь, с акцентом на гармонию с природой и создание благоприятной атмосферы для жителей."
+	}
+];
+
 export default function Partners() {
 	const { breakpoints } = useTheme();
 	const isMobile = useMediaQuery(breakpoints.down("md"));
@@ -24,7 +82,13 @@ export default function Partners() {
 		>
 			<Box sx={{ display: "flex", flexDirection: "column", width: "100%" }}>
 				<Title text='Партнёры' />
-				<PartnerCard
+				{partners.map((partner, index, self) => {
+					const isLast = index === self.length - 1;
+					return (
+						<PartnerCard key={partner.name} image={partner.image} name={partner.name} description={partner.description} type='' divider={!isLast} />
+					);
+				})}
+				{/* <PartnerCard
 					image={{
 						src: "/images/baltia.png",
 						width: 1099,
@@ -56,7 +120,7 @@ export default function Partners() {
 					name='Бэл девелопмент'
 					description={`Профессиональная девелоперская компания полного цикла создана в 2002 году и уже более двух десятилетий строит высококлассные жилые и коммерческие объекты в России и странах СНГ`}
 					type=''
-				/>
+				/> */}
 			</Box>
 		</Box>
 	);
