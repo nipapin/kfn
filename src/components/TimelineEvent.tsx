120f19f16ca6ed584cba147c5c93c417c07c8644
nipapin <papin201212@gmail.com>
2026-06-25 13:50:26 +0200

    Update TimelineEvent component typography and enhance event program details for the 2026 forum. Adjust font size for better readability and add new hall information with detailed content for each session, including speakers and descriptions.

--- src/components/TimelineEvent.tsx
+++ src/components/TimelineEvent.tsx
@@ -6,7 +6,7 @@ import EventCard from "./EventCard";
 export default function TimelineEvent({ hall }: { hall: Hall }) {
 	return (
 		<Stack direction={"column"} gap={"1rem"} width={"100%"}>
-			<Typography fontSize={{ lg: "1.5rem", xs: "1rem" }} fontWeight={500} sx={{ display: "flex", alignItems: "center", gap: "0.5rem" }}>
+			<Typography fontSize={{ lg: "1.25rem", xs: "1rem" }} fontWeight={500} sx={{ display: "flex", alignItems: "center", gap: "0.5rem" }}>
 				<Place color='primary' /> {hall.name}
 			</Typography>
 			{hall.content.map((program, index) => (
